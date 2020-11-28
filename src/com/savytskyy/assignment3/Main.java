097e689726bed934470595b121ae7e1b633be044
Andriy Savytskyy <andy1978dnepr@gmail.com>
2020-11-28 12:05:16 +0200

    improved Add method

--- src/com/savytskyy/assignment3/Main.java
+++ src/com/savytskyy/assignment3/Main.java
@@ -7,9 +7,38 @@ public class Main {
         PointList pointList = new PointList();
         pointList.add(new Point(1,5));
         pointList.add(new Point(2,6));
+        pointList.add(new Point(2,6));
+        pointList.add(new Point(4,6));
+        pointList.add(new Point(2,6));
+        pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+        //pointList.add(new Point(2,6));
+
+
+
+        for (int i = 0; i < pointList.getLastElementIndex(); i++) {
+            System.out.println(pointList.get(i));
+        }
+
+        System.out.println();
+        System.out.println();
 
         for (int i = 0; i < pointList.size(); i++) {
                 System.out.println(pointList.get(i));
         }
+
+
+
+
+
+
+
+
     }
 }
--- src/com/savytskyy/assignment3/PointList.java
+++ src/com/savytskyy/assignment3/PointList.java
@@ -1,11 +1,9 @@
 package com.savytskyy.assignment3;
 
-import java.util.Arrays;
-
 public class PointList {
 
-    int lastElement = 0;
-    int capacity = 4;
+    private int lastElement = 0;
+    private int capacity = 4;
     Point[] points = new Point[capacity];
 
 
@@ -13,26 +11,35 @@ public class PointList {
         //points = Arrays.copyOf(points, points.length+1);
         //points[points.length-1] = p;
 
-        Point[] buffer = new Point[points.length + 1];
-        for (int i = 0; i < points.length; i++) {
+        points[lastElement] = p;
+        lastElement++;
+
+        if (lastElement == capacity-1) {
+            capacity = (int) (capacity *1.5);
+            this.arrayResize((int) (capacity));
+        }
+    }
+
+    public void arrayResize (int size) {
+        Point[] buffer = new Point[size];
+        for (int i = 0; i <= lastElement; i++) {
             buffer[i] = points[i];
         }
-        buffer[buffer.length - 1] = p;
         points = buffer;
     }
 
     public Point get(int index) {
-        checkValidIndex(index);
+        this.checkValidIndex(index);
         return points[index];
     }
 
     public void set(Point p, int index) {
-        checkValidIndex(index);
+        this.checkValidIndex(index);
         this.points[index] = p;
     }
 
     public void remove(int index) {
-        checkValidIndex(index);
+        this.checkValidIndex(index);
 
         Point[] buffer = new Point[points.length - 1];
         for (int i = 0; i < buffer.length; i++) {
@@ -43,6 +50,10 @@ public class PointList {
 
     public int size () {
         return points.length;
+    }
+
+    public int getLastElementIndex () {
+        return lastElement;
     }
 
 
